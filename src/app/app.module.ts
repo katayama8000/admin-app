520cc731dfcd2a22afba08c8fadecfcc49c0b473
katayama <tattu.0310@gmail.com>
2022-07-07 13:43:06 +0900

    ✨ メンバー詳細コンポーネント追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MenbersComponent } from './menbers/menbers.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, MenbersComponent],
+  declarations: [AppComponent, MenbersComponent, MemberDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
