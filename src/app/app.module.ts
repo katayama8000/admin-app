61ab16fed448eab596870a4ceb244569a7fc62de
katayama <tattu.0310@gmail.com>
2022-07-07 11:41:57 +0900

    ✨ formを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MenbersComponent } from './menbers/menbers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenbersComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MenbersComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
