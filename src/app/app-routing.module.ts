0fcc0686dc0daaf748ed445876c1d77cd1809da2
katayama <tattu.0310@gmail.com>
2022-07-07 18:34:23 +0900

    ✨ router実装

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
+import { MembersComponent } from './members/members.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from "@angular/router"
 
-
+const routes: Routes = [
+  {path:"members",component:MembersComponent}
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
