78e3427933dd4f7471c129c656b2f9dd6cb3d460
katayama <tattu.0310@gmail.com>
2022-07-13 00:11:16 +0900

    ✨ 検索

---
+++ src/app/member-search/member-search.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { Member } from '../member';
+import { MemberService } from '../member.service';
+
+@Component({
+  selector: 'app-member-search',
+  templateUrl: './member-search.component.html',
+  styleUrls: ['./member-search.component.css'],
+})
+export class MemberSearchComponent implements OnInit {
+  members$: Observable<Member[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private memberService: MemberService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {this.members$ = this.searchTerms.pipe(
+    // wait 300ms after each keystroke before considering the term
+    debounceTime(300),
+    // ignore new term if same as previous term
+    distinctUntilChanged(),
+    // switch to new search observable each time the term changes
+    switchMap((term: string) => this.memberService.searchMembers(term))
+  );}
+}
