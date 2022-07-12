8892c62715e92a7f804c70f7eddac7f6b7bd6442
katayama <tattu.0310@gmail.com>
2022-07-12 11:03:58 +0900

    ✨ 更新

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -3,7 +3,7 @@ import { Member } from './member';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError,map,tap} from 'rxjs/operators';
 
 @Injectable({
@@ -11,6 +11,9 @@ import { catchError,map,tap} from 'rxjs/operators';
 })
 export class MemberService {
   private membersUrl = 'api/members';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -26,7 +29,19 @@ export class MemberService {
 
   getMember(id:number): Observable<Member> {
     this.messageService.add(`MemberService:社員一覧データ(${id})を取得しました`);
-    return of(MEMBERS.find(member => member.id === id));
+    //return of(MEMBERS.find(member => member.id === id));
+    const url = `${this.membersUrl}/${id}`;
+    return this.http.get<Member>(url).pipe(
+      tap(_ => this.log(`社員データ(${id})を取得しました`)),
+      catchError(this.handleError<Member>(`getMember id=${id}`))
+    )
+  }
+
+  updateMember(member: Member): Observable<any> {
+    return this.http.put(this.membersUrl, member, this.httpOptions).pipe(
+      tap(_ => this.log(`社員データ(${member.id})を更新しました`)),
+      catchError(this.handleError<any>('updateMember'))
+    );
   }
 
   private log(message: string) {
